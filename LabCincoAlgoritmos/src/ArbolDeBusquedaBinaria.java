1a1aa451c4499436135967c1bb74d85d0d21c23a
jesusCS2000 <castillosalazarjesus30@gmail.com>
2023-06-07 00:47:38 -0600

    ClaseCarroCompleta

--- LabCincoAlgoritmos/src/ArbolDeBusquedaBinaria.java
+++ LabCincoAlgoritmos/src/ArbolDeBusquedaBinaria.java
@@ -22,10 +22,10 @@ ArbolDeBusquedaBinaria(){
         entradaOpcion();
     }
 
-    public void entradaOpcion(){
+    public void entradaOpcion() {
         int opcion = entrada.nextInt();
-        while(opcion>=1 || opcion<6){
-            switch (opcion){
+        while (opcion >= 1 || opcion < 6) {
+            switch (opcion) {
                 case 1:
                     //Insertar un carro
                 case 2:
@@ -37,64 +37,42 @@ ArbolDeBusquedaBinaria(){
                 case 5:
                     //PostOrden
 
-            // si la entrada 6 sale completamente
-            if (opcion == 6) {
-                System.out.println("HA SALIDO DEL MENÚ");
-                break;
+                    // si la entrada 6 sale completamente
+                    if (opcion == 6) {
+                        System.out.println("HA SALIDO DEL MENÚ");
+                        break;
+                    }
+                    System.out.println("\n| | | INGRESE OTRA OPCIÓN | | | ");
+                    opcion = entrada.nextInt();
             }
-            System.out.println("\n| | | INGRESE OTRA OPCIÓN | | | ");
-            opcion = entrada.nextInt();
+        }
+
+        public void carroInfo() {
+            entrada.nextLine();
+
+            System.out.println("Ingrese un id o placa para el carro");
+            int idPlaca = entrada.nextInt();
+            System.out.println("idPlaca es: " + idPlaca + "\n");
+
+            System.out.println("Ingrese el modelo del carro ");
+            String modelo = entrada.next();
+            System.out.println("Modelo: " + modelo + "\n");
+
+            System.out.println("Ingrese el año del carro ");
+            int year = entrada.nextInt();
+            System.out.println("El año del carro es:" + year + "\n");
+
+            System.out.println("Ingrese el color del carro ");
+            String color = entrada.next();
+            System.out.println("El color del carro es: " + color + "\n");
+
+            Carro car = new Carro(idPlaca, modelo, year, color);
         }
     }
 
-    public void estudianteInfo(){
-                entrada.nextLine();
-
-                System.out.println("Ingrese un id o placa para el carro");
-                int idPlaca = entrada.nextInt();
-                System.out.println("idPlaca es: " + idPlaca + "\n");
-
-                System.out.println("Ingrese el modelo del carro ");
-                String modelo = entrada.next();
-                System.out.println("Modelo: " + modelo + "\n");
-
-                System.out.println("Ingrese el año del carro ");
-                int year = entrada.nextInt();
-                System.out.println("El año del carro es:" + year + "\n");
-
-                System.out.println("Ingrese el color del carro ");
-                String color = entrada.next();
-                System.out.println("El color del carro es: " + color  + "\n");
-
-                Carro carro = new Carro(idPlaca, modelo, year, color);
-               // append(carro);
-            }
 
 
 
-
-
-
-    /*
-    public void insert(int idPlaca){
-        raiz = insertarCarro(raiz, idPlaca);
-    }
-
-    Node insertarCarro(Node raiz, int idPlaca){
-        if(raiz == null){
-            raiz = new Node(idPlaca);
-            return raiz;
-        }
-        if(idPlaca < raiz.idPlaca)
-            raiz.izquierdo = insertarCarro(raiz.izquierdo, idPlaca);
-        else if (raiz > raiz.idPlaca)) {
-            raiz.derecho = insertarCarro(raiz.derecho, idPlaca);
-        }
-        return raiz;
-    }
-
-    */
-    }
 
 
     }

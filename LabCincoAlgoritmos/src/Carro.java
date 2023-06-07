44f823d137bcaa9be1f5c076c19f82347d480765
jesusCS2000 <castillosalazarjesus30@gmail.com>
2023-06-06 22:44:49 -0600

    ClaseCarroCompleta

--- LabCincoAlgoritmos/src/Carro.java
+++ LabCincoAlgoritmos/src/Carro.java
@@ -1,3 +1,29 @@
 public class Carro {
+int idPlaca;
+String modelo;
+int year;
+String color;
+
+Carro izquierdo, derecho;
+
+    public Carro(int idPlaca, String modelo, int year, String color, Carro izquierdo, Carro derecho) {
+        this.idPlaca = idPlaca;
+        this.modelo = modelo;
+        this.year = year;
+        this.color = color;
+        this.izquierdo = izquierdo;
+        this.derecho = derecho;
+    }
+
+    public String toString(){
+        return "Carro{" +
+                "idPlaca=" + idPlaca +
+                ", modelo=" + modelo +
+                ", year=" + year +
+                ", color=" + color +
+                "}";
+    }
+
 
 }
+
